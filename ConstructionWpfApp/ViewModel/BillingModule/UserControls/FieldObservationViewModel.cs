5b1cd232d3eb77ccd65d2b9ec54f96852c19e2ed
Sourodeep Chatterjee <sourodeep224@outlook.com>
2019-03-30 20:50:16 +0530

    FieldObservationViewModel added

---
+++ ConstructionWpfApp/ViewModel/BillingModule/UserControls/FieldObservationViewModel.cs
@@ -0,0 +1,30 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="FieldObservationViewModel.cs" company="SDCWORLD">
+//   Sourodeep Chatterjee
+// </copyright>
+// <summary>
+//   Defines the FieldObservationViewModel type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ConstructionWpfApp.ViewModel.BillingModule.UserControls
+{
+    using System;
+    using System.Net;
+    using System.Windows;
+
+    using ConstructionWpfApp.Helper;
+
+    /// <summary>
+    /// The field observation view model.
+    /// </summary>
+    public class FieldObservationViewModel
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FieldObservationViewModel"/> class.
+        /// </summary>
+        public FieldObservationViewModel()
+        {
+        }
+    }
+}
